a076e545119004dc6a62c4c7fbeb9e9868606682
mayasnayak <mayashankarnayak@gmail.com>
2020-06-27 08:59:20 +0530

    Update updatebot.sh

--- updatebot.sh
+++ updatebot.sh
@@ -2,3 +2,5 @@
 jx step create pr go --name github.com/jenkins-x/go-scm --version ${VERSION} --build "make mod" --repo https://github.com/jenkins-x/lighthouse.git
 jx step create pr go --name github.com/jenkins-x/go-scm --version ${VERSION} --build "make mod" --repo https://github.com/cloudbees/jx-tenant-service.git
 jx step create pr go --name github.com/jenkins-x/go-scm --version ${VERSION} --build "make mod" --repo https://github.com/cloudbees/lighthouse-githubapp.git
+
+# this is a comment
